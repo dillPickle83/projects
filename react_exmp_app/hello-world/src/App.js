c67e271bd2248e48670004dbd9dce70e3c31d3ca
barnacles <sp503488@gmail.com>
2025-06-27 03:28:01 -0500

    state, functional comps, class comps, jsx'

--- react_exmp_app/hello-world/src/App.js
+++ react_exmp_app/hello-world/src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import { Greet } from './components/greet'
+import Welcome from './components/welcome'
+import Hello from './components/hello'
+import Message from './components/message'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-		Hello World!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+	<Greet name="Joji" alterEgo="Filthy Frank"/>
+			<p>Its filthy Frank</p>
+	<Greet name="Elliot" alterEgo="Mr.Robot"/>
+			<button>Hecking time</button>
+	<Welcome name="Joji" alterEgo="Filthy Frank"/>
+	<Hello />
+	<Message />
     </div>
   );
 }
