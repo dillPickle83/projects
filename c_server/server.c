e250010eeebec34bb86cefbbe26745a9e0918fac
sandesh <sp503488@gmail.com>
2026-03-12 15:20:08 +0000

    added while loop for the program to stay alive

--- c_server/server.c
+++ c_server/server.c
@@ -38,24 +38,41 @@ int main(){
 
     printf("Server is listening on port %d\n", PORT);
 
-    // accept the connection
-    new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t *)&addrlen);
-    if(new_socket < 0){
-        perror("ACCEPT FAILED");
-        exit(EXIT_FAILURE);
+    while(1){
+        // accept the connection
+        new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t *)&addrlen);
+        if(new_socket < 0){
+            perror("ACCEPT FAILED");
+            exit(EXIT_FAILURE);
+        }
+    
+        // Clear the buffer before reading new data
+        memset(buffer, 0, BUFFER_SIZE);
+
+        // read data from the client
+        read(new_socket, buffer, BUFFER_SIZE);
+        printf("---- New request received ----\n%s\n", buffer);
+
+        // send response to the client
+        char *html_body = "<html><body><h1>Firmware Dev API Online</h1><p>Ready for embedded systems requests!</p></body></html>";
+        char response[2048];
+        snprintf(response, sizeof(response),
+                 "HTTP/1.1 200 OK\r\n"
+                 "Content-Type: text/html\r\n"
+                 "Content-Length: %lu\r\n"
+                 "\r\n"
+                 "%s",
+                 strlen(html_body), html_body);
+
+        // Send the complete HTTP response back to the client
+        write(new_socket, response, strlen(response));
+        printf("Response sent to client:\n%s\n", response);
+
+        // close the socket
+        close(new_socket);
     }
 
-    // read data from the client
-    read(new_socket, buffer, BUFFER_SIZE);
-    printf("Received from client:\n%s\n", buffer);
-
-    // send response to the client
-    char *response = "Hello from the other side!";
-    write(new_socket, response, strlen(response));
-    printf("Response sent to client:\n%s\n", response);
-
-    // close the socket
-    close(new_socket);
+    // Close the server when the while loop is exited (^C)
     close(server_fd);
 
     return 0;
